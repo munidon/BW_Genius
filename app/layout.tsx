6e3553dbd5b28f44827c4fc34ecc33fd19b7c433
황현종 <munidon@naver.com>
2026-03-08 00:25:46 +0900

    Add 애드센스 메타와 배너 자리

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-5091515187087145" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5091515187087145"
